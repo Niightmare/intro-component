ced28331284426f77688e4732d9931248da909cc
Niightmare <domgz.alan@gmail.com>
2020-08-11 19:15:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
-const submit = document.getElementById('btn-submit'); 
-const firstName = document.getElementById('first-name'); 
-const lastName = document.getElementById('last-name'); 
-const email = document.getElementById('email'); 
-const password = document.getElementById('password');
+var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-function validateEmail(email) {
-  const regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regEx.test(email);
+function validate() {
+    let firstName = document.querySelector("#first-name");
+    let next = firstName.nextElementSibling;
+    firstName = firstName.value.trim();
+
+    let lastName = document.querySelector("#last-name");
+    let nextl = lastName.nextElementSibling;
+    lastName = lastName.value.trim();
+
+    let password = document.querySelector("#password");
+    let nextp = password.nextElementSibling;
+    password = password.value.trim();
+
+    let email = document.querySelector("#email");
+    let nexte = email.nextElementSibling;
+
+    (firstName == "") ? (next.classList.add('fa-error'), next.nextElementSibling.classList.add("message-error")) : (next.classList.remove("fa-error"), next.nextElementSibling.classList.remove("message-error")) ;
+    (lastName == "") ? (nextl.classList.add('fa-error'), nextl.nextElementSibling.classList.add("message-error")) : (nextl.classList.remove("fa-error"), nextl.nextElementSibling.classList.remove("message-error")) ;
+    (password == "") ? (nextp.classList.add('fa-error'), nextp.nextElementSibling.classList.add("message-error")) : (nextp.classList.remove("fa-error"), nextp.nextElementSibling.classList.remove("message-error")) ;
+
+    let res = re.test(String(email).toLowerCase());
+
+    console.log(res);
+
+
+
+  
+//   var form = document.forms["register-form"].getElementsByTagName("input");
+//   var form = document.getElementById("register-form").elements;
+
+
+
+//   for (var i = 0; i < form.length; i++) {
+//     console.log(i.value);
+//   }
 }
-
-submit.addEventListener('submit', () => {
-    console.log("Hola");
-});
