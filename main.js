eebe006a202cf044fe445094230ac1b2549d016f
Niightmare <domgz.alan@gmail.com>
2020-08-12 19:30:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,31 @@
-var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function validateEmail(email) {
+  let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
+}
+
+function validateForm(elements) {
+  for (i = 0; i < elements.length; i++) {
+    let input_id = elements[i].id;
+    let input = document.querySelector(`#${input_id}`);
+    let message = input.nextElementSibling;
+
+    if (input_id != "email") {
+      input.value == ""
+        ? (message.classList.add("fa-error"),
+          message.nextElementSibling.classList.add("message-error"))
+        : (message.classList.remove("fa-error"),
+          message.nextElementSibling.classList.remove("message-error"));
+    } else {
+      !validateEmail(input.value)
+        ? (message.classList.add("fa-error"),
+          message.nextElementSibling.classList.add("message-error"))
+        : (message.classList.remove("fa-error"),
+          message.nextElementSibling.classList.remove("message-error"));
+    }
+  }
+}
 
 function validate() {
-    let firstName = document.querySelector("#first-name");
-    let next = firstName.nextElementSibling;
-    firstName = firstName.value.trim();
-
-    let lastName = document.querySelector("#last-name");
-    let nextl = lastName.nextElementSibling;
-    lastName = lastName.value.trim();
-
-    let password = document.querySelector("#password");
-    let nextp = password.nextElementSibling;
-    password = password.value.trim();
-
-    let email = document.querySelector("#email");
-    let nexte = email.nextElementSibling;
-
-    (firstName == "") ? (next.classList.add('fa-error'), next.nextElementSibling.classList.add("message-error")) : (next.classList.remove("fa-error"), next.nextElementSibling.classList.remove("message-error")) ;
-    (lastName == "") ? (nextl.classList.add('fa-error'), nextl.nextElementSibling.classList.add("message-error")) : (nextl.classList.remove("fa-error"), nextl.nextElementSibling.classList.remove("message-error")) ;
-    (password == "") ? (nextp.classList.add('fa-error'), nextp.nextElementSibling.classList.add("message-error")) : (nextp.classList.remove("fa-error"), nextp.nextElementSibling.classList.remove("message-error")) ;
-
-    let res = re.test(String(email).toLowerCase());
-
-    console.log(res);
-
-
-
-  
-//   var form = document.forms["register-form"].getElementsByTagName("input");
-//   var form = document.getElementById("register-form").elements;
-
-
-
-//   for (var i = 0; i < form.length; i++) {
-//     console.log(i.value);
-//   }
+  var inputs = document.getElementById("register-form").elements;
+  validateForm(inputs);
 }
